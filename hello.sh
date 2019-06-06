33e994ceb9d30717cae05cad6c83f10c3091ac5f
Sedrickgael <anangaman.kouagni@uvci.edu.ci>
2019-06-06 13:59:29 +0000

    My very first commit

--- hello.sh
+++ hello.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-name=$(curl https://api.github.com/users/SedrickGael | grep '"login":' | sed -e 's/^.*": "//g' -e 's/",.*$//g')
+name=$(curl https://api.github.com/users/Sedrickgael | grep '"login":' | sed -e 's/^.*": "//g' -e 's/",.*$//g')
 echo "nom: $name"
