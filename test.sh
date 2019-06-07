2cb2e5a6d750530857572079a1b9bd286721422c
Sedrickgael <anangaman.kouagni@uvci.edu.ci>
2019-06-07 17:08:20 +0000

    level 2

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash/
+mkdir 0
+chmod 401 0
+touch -t 8601050000 0
+touch -t 8611130001 1
+chmod 402 1
+touch -t 8803050010 2
+chmod 604 2
+ln -s 0 3
+touch -h 3 -t 9002160011
+touch -t 9010070100 4
+chmod 510 4
+touch -t 9011070101 5
+chmod 460 5
+touch -t 9102080110 6
+chmod 460 6
+touch -t 9103080111 7
+chmod 510 7
+touch -t 9405201000 8
+chmod 604 8
+touch -t 9406101001 9
+chmod 402 9
+mkdir A
+touch -t 9501101010 A
+chmod 401 A
