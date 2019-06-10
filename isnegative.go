e7cd363e2bacf0d095767896f7fbc57eb6793777
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-10 17:48:23 +0000

    Update isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -1,10 +1,11 @@
-package main
+package piscine
 
 import "fmt"
 
-func IsNegative (x int) {
-	if x >=0 {
-		fmt.Println("F")
+
+func IsNegative (nb int) {
+	if nb >=0 {
+	  fmt.Println("F")
 	} else {
 		fmt.Println("T")
 	}
