d89eacd6a3ca416a2c813e57dd3ac0946510d486
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 13:03:31 +0000

    Update iterativefactoriel.go

--- iterativefactoriel.go
+++ iterativefactoriel.go
@@ -2,10 +2,13 @@ package piscine
 
 func IterativeFactorial(nbr int) int {
 	fac := 1
-	if(nbr<0){
+	if nbr<0 {
 		fac=0
 	}
-	for i:=nbr; i>=1; i-- {
+	if nbr=0 || nbr = 1 {
+		fac = 1
+	}
+	for i:=nbr; i>1; i-- {
 		fac *= i
 	}	
 	return fac
