38870a90abc64f70886601b1b302fd3a56a5ed31
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 14:24:35 +0000

    Create sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "math"
+
+func Sqrt(nbr int) int {
+	for i := 0; i < 101; i++ {
+		if nbr == i*i {
+			nbr = i
+			break
+		} else if i*i > nbr {
+			nbr = 0
+			break
+		}
+	}
+	return nb
+}
