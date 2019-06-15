7b9e6c84757b5b743480d63b247a5840ad2250c7
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 13:50:25 +0000

    Update iterativepower.go

--- iterativepower.go
+++ iterativepower.go
@@ -2,12 +2,12 @@ package piscine
 
 func IterativePower(nbr int, power int)int {
 	pow :=1
-	if ( (power>0) {
+	if power>0 {
 		for i:=power; i>=1; i--{
 			pow *=nbr
 		}
 		
-	} else if (power==0){
+	} else if power==0 {
 		pow = 1
 	} else {
 		pow = 0
