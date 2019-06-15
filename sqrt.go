a6c8f05ed08b721042d79166b5865f24ef3caee5
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 14:39:34 +0000

    Update sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -2,18 +2,20 @@ package piscine
 
 import "math"
 
-func Sqrt(nbr int) int {
-	if nbr < 0 || 256 < nbr {
+func Sqrt(nb int) int {
+	if nb < 0 || math.MaxInt32 < nb {
 		return 0
-	}
-	for i := 0; i < 101; i++ {
-		if nbr == i*i {
-			nbr = i
-			break
-		} else if i*i > nbr {
-			nbr = 0
-			break
+	}else {
+		
+		for i := 0; i < 101; i++ {
+			if nb == i*i {
+				nb = i
+				break
+			} else if i*i > nb {
+				nb = 0
+				break
+			}
 		}
 	}
-	return nbr
+	return nb
 }
