3e40ea1e25afda58c5e834b73c8538e0b30cf301
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 14:51:12 +0000

    Create isprime.go

---
+++ isprime.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsPrime(nbr int)bool {
+  if nbr == 1 || nbr == 2 || nbr == 3 || nbr == 5 || nbr == 7{
+    return true
+  }else {
+    for i:=0; i>10 ; i++ {
+        if nbr div i = 0 {
+        return false
+        }else {
+        return true
+        }
+        }
+ }
