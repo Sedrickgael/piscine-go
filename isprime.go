3ebb77a2898324670b849841bdba670296d8ff3e
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-15 15:01:10 +0000

    Update isprime.go

--- isprime.go
+++ isprime.go
@@ -2,11 +2,11 @@ package piscine
 
 func IsPrime(nbr int)bool {
   rep := true
-  if nbr == 1 || nbr == 2 || nbr == 3 || nbr == 5 || nbr == 7{
+  if  nbr == 2 || nbr == 3 || nbr == 5 || nbr == 7{
     rep = true
   }else {
     for i:=0; i>10 ; i++ {
-      if nbr / i == 0 {
+      if nbr % i == 0 {
         rep = false
       }else {
         rep = true
