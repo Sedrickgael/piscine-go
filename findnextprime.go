4a82aa15fa02c6acfad3f79da16233567a3d7e0d
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-16 12:34:34 +0000

    Update findnextprime.go

--- findnextprime.go
+++ findnextprime.go
@@ -1,17 +1,31 @@
 package piscine
 
-func FindNextPrime(nbr int)int {
-  next := nbr
+func Prime(nbr int)bool {
+  rep := true
   if  nbr == 2 || nbr == 3  || nbr == 5 || nbr == 7 || nbr == 11 || nbr == 13 || nbr == 17 || nbr == 19 || nbr == 21 || nbr == 23 || nbr == 29 || nbr == 31 || nbr == 37 {
-    next = nbr
+    rep = true
   }else if nbr == 1 || nbr ==0 {
-    next = 2
+    rep = false
   }else {
     for i:= 2; i<40; i++ {
-      if nbr % i != 0{
-        next = nbr
+      if nbr % i == 0{
+        rep = false
       }
-	  }
+	}
+	
   }
-  return next
+  return rep
+}
+
+
+func FindNextPrime(nbr int)int {
+	next := nbr 
+	for i= nbr; i>nbr;  {
+		if Prime(i){
+			next=i
+		}else {
+			i++
+		}
+	}
+	return next
 }
