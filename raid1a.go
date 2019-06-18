ca01385b39eb38c1ff4efe119dd9e4f6395e1c31
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-18 12:02:31 +0000

    Create raid1a.go

---
+++ raid1a.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func Raid1a(x, y int){
+	for i:=1; i<=x; i++{
+		if i==1 || i==x{
+			fmt.Print("0")
+		}else {
+			fmt.Print("-")
+		}		
+	}
+	if y>1 {
+		fmt.Print("\n")
+	}
+	
+	for j:=2; j<=y-1 ; j++{
+		if x == 1 {
+			fmt.Println("|")
+		}else {
+			fmt.Print("|")
+			for i:=1; i<=x-2; i++{
+				fmt.Print(" ")	
+			}
+			fmt.Println("|")
+		}
+		
+		
+	}
+	if y>1{
+		for i:=1; i<=x; i++{
+			if i==1 || i==x{
+				fmt.Print("0")
+			}else {
+				fmt.Print("-")
+			}		
+		}
+	}
+	
+}
