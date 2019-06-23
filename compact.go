d5369f6e38053d8335205003414ed4d7751b9f47
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-23 14:56:41 +0000

    Update compact.go

--- compact.go
+++ compact.go
@@ -1,9 +1,9 @@
 package piscine
 
 func Compact(ptr *[]string, length int) int {
-	count:=0
-	for i:=0;i<length;i++{
-		if (*ptr)[i]!=" "{
+	count := 0
+	for i := 0; i < length; i++ {
+		if (*ptr)[i] != " " {
 			count++
 		}
 	}
