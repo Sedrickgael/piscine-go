58e36c3169ecb2f09e6e998321ded38230910afe
Sedrickgael <36696703+Sedrickgael@users.noreply.github.com>
2019-06-23 14:34:36 +0000

    Update main.go

--- comcheck/main.go
+++ comcheck/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var arrayRune []rune
 	var result string
-	safeWords := []string{"Alert!!!", "Alert!!!", "Alert!!!"}
+	safeWords := []string{"01", "galaxy", "galaxy 01"}
 
 	for i := 1; i < len(os.Args); i++ {
 		arrayRune = []rune(os.Args[i])
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	for _, s := range safeWords {
-		if result == s {
+		if result != s {
 			fmt.Println("Alert!!!")
 		}
 	}
